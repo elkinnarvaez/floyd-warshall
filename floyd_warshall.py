27202f17ad5c91c2967ea57a72c03279e199f940
Elkin Narvaez Paz <elkinnarvaez2406@gmail.com>
2021-06-03 19:32:16 -0500

    Parallel implementation in process

--- floyd_warshall.py
+++ floyd_warshall.py
@@ -3,8 +3,35 @@ import random
 import datetime
 import time
 from timeit import default_timer
+import numpy as np
+import pycuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+import pycuda.driver as drv
 
-dis = [[None for _ in range(1001)] for _ in range(1001)]
+MAX_N = 1000
+dis = np.array([[0 for _ in range(MAX_N)] for _ in range(MAX_N)], dtype='int')
+
+ker = SourceModule("""
+    #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+    __global__ void calculate_kernel(int *dis, int n, int k){
+        int i = threadIdx.x, j;
+        for(j = 0; j < n; j++){
+            dis[i * n + j] = MIN(dis[i * n + j], dis[i * n + k] + dis[i * n + j]);
+        }
+    }
+    """)
+
+# ker = SourceModule("""
+#     __global__ void calculate_kernel(int *dis, int n, int k){
+#         int i = threadIdx.x, j;
+#         for(j = 0; j < n; j++){
+#             dis[i] = %(MAX_N)i;
+#         }
+#     }
+#     """%{'MAX_N': MAX_N})
+
+calculate_kernel = ker.get_function("calculate_kernel")
 
 def floyd_warshall_sequential(n):
     for k in range(n):
@@ -12,13 +39,18 @@ def floyd_warshall_sequential(n):
             for j in range(n):
                 dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j])
 
+def floyd_warshall_parallel(n):
+    dis_gpu = pycuda.gpuarray.to_gpu(dis)
+    for k in range(n):
+        calculate_kernel(dis, n, k, block = (n, 1, 1), grid = (1, 1, 1))
+
 def generate_randon_graph(n):
     for i in range(n):
         for j in range(n):
             if(i == j):
-                dis[i][j] = 0
+                dis[i][j] = np.int64(0)
             else:
-                dis[i][j] = random.randint(1, 11)
+                dis[i][j] = np.int64(random.randint(1, 11))
     return dis
 
 def multiple_examples_running_time():
@@ -60,6 +92,7 @@ def individual_example_running_time():
             
 def main():
     individual_example_running_time()
+    return 0
 
 if __name__ == '__main__':
     main()
